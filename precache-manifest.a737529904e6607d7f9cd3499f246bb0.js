ed7b9bfcb40a9d72dfed8da854b6e3f2cf94df62
AdithyaSireesh <s1616497@sms.ed.ac.uk>
2019-10-16 22:19:10 +0100

    Updates

---
+++ precache-manifest.a737529904e6607d7f9cd3499f246bb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beeaceed19a36f8f234f1c0daebf6e88",
+    "url": "/my_website/index.html"
+  },
+  {
+    "revision": "e7c8f268549f3b90d6eb",
+    "url": "/my_website/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f402f876554cecc469ef",
+    "url": "/my_website/static/js/2.58b4a2e2.chunk.js"
+  },
+  {
+    "revision": "e7c8f268549f3b90d6eb",
+    "url": "/my_website/static/js/main.85918efd.chunk.js"
+  },
+  {
+    "revision": "c1de47df2b2f43aa9e18",
+    "url": "/my_website/static/js/runtime~main.3abc96d5.js"
+  }
+]);
